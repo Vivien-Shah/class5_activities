ed274f72f0ac2895f53622f781b02b1bdd053ae2
Vivien-Shah <hfsthane7787@gmail.com>
2025-05-23 19:24:28 +0530

    Add files via upload

---
+++ class5.py
@@ -0,0 +1,35 @@
+'''num=int(input("Enter the number: "))
+if num>50:
+    print(num,"is greater than 50")
+else: 
+    print(num,"lesser than 50")'''
+
+
+
+'''Activity 1
+num=int(input("enter the number: "))
+if num>0:
+    print(num,"positive number")'''
+
+
+'''activity2
+actual_cost=int(input("Enter the actual price: "))
+selling_cost=int(input("Enter the selling amount: "))
+if (selling_cost>actual_cost ): 
+    profit=selling_cost-actual_cost
+    print("my profit amount is :",profit)
+else: 
+    loss=actual_cost-selling_cost
+    print("my loss amount is:",loss)'''
+
+# Activity 4
+number=int(input("Enter the number to check: "))
+print("Number to be checked: ",number)
+ 
+if number%2==0: 
+    print("This is an even number")
+else:
+    print("This is an odd number")
+
+
+
